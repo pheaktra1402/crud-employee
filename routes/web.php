603b8e5ac9e811dfc16f3d27cabcb017fd76e5a6
pheaktra1402 <pheaktrathorn474@gmail.com>
2026-09-25 15:30:07 +0700

    employee route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/employee',[EmployeeController::class,'index'])->name('employee.index');
